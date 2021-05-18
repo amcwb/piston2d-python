2bcf4f3f18f53d395db9f93bd313e95ff7b30657
Avery <19372006+starsflower@users.noreply.github.com>
2021-05-18 22:57:03 +0100

    fix: Add piston2d.window subpackage to setup.py [skip docs] [skip build]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="piston2d",
     version="0.1.6",
     rust_extensions=[RustExtension("piston2d.piston2d", binding=Binding.PyO3)],
-    packages=["piston2d"],
+    packages=["piston2d", "piston2d.window"],
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
 )
